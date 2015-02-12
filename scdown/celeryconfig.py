762fef0d3a26afb9e1476830952733e94750d65c
Chris Johnson-Roberson <chris.johnson.roberson@gmail.com>
2015-02-11 22:23:41 -0500

    parse mongodb uri

--- scdown/celeryconfig.py
+++ scdown/celeryconfig.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 BROKER_URL = os.getenv("CLOUDAMQP_URL", 'amqp://')
 # BROKER_POOL_LIMIT = None
@@ -8,12 +9,16 @@ MONGOLAB_DB = None
 
 mongolab = os.getenv("MONGOLAB_URI")
 if mongolab is not None:
-    uri, _, db = mongolab.rpartition('/')
+    uri_pat = r"mongodb://([^:]+):([^@]+)@([^:]+):(\d+)/(.+)"
+    user, passwd, host, port, db = re.match(uri_pat, mongolab).groups()
+    uri = "mongodb://{}:{}".format(host, port)
     MONGOLAB_URI = uri
     MONGOLAB_DB = db
     CELERY_RESULT_BACKEND = uri
     CELERY_MONGODB_BACKEND_SETTINGS = {
-      'database': db,
+        'database': db,
+        'user': user,
+        'password': passwd
     }
 
 CELERY_TASK_SERIALIZER = 'json'
